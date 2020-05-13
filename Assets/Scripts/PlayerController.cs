5ff5d951e5bd584fbf470747e3a29ecb919b5147
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2020-05-12 19:41:17 -0500

    task 1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        // Get info from User to move in the X and Y axis
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3 (horizontalInput, 0.0f, verticalInput);
+        
+        rb.AddForce(movement * speed);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
